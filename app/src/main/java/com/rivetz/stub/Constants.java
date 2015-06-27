5e681e30c0967d97af5b19d174dcb56c4bdc8a03
hobnobber <markdavid@hoblit.net>
2015-06-27 13:01:11 -0400

    rivetzspid constant

--- app/src/main/java/com/rivetz/stub/Constants.java
+++ app/src/main/java/com/rivetz/stub/Constants.java
@@ -20,6 +20,8 @@ public class Constants {
     public static final boolean PROGRESS_INDICATOR_ENABLED = true;
     // Service Provider ID for the Developer Account
     public static final String DEVELOPER_SPID = "029d785242baad9f3d7bedcfca29d5391b3c247a3d4eaf5c3a0a5edd9489d1fcad";
+    // public static final String RIVETZ_SPID = "029755B26A5A1992B6C2CA1F732A0CBBDCE9D08CB6FC00CBEAC63F4078B5AA3950";
+    public static final String RIVETZ_SPID = "029755b26a5a1992b6c2ca1f732a0cbbdce9d08cb6fc00cbeac63f4078b5aa3950";
     // the name of the file for writing and reading the TA code
     public static final String TAFILENAME = "ta.bin";
 
